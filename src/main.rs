63e42325c8ca035f36143bf7f8eef6b68d0ce7c3
Fredrik Simonsson <simson@thesimson.net>
2018-11-20 00:44:01 +0900

    Write to pin connected to LED

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,10 @@ use cortex_m::peripheral::syst::SystClkSource;
 use cortex_m_rt::entry;
 // use cortex_m_semihosting::hprint;
 
-#[feature(unproven)]
-#[cfg(feature = "unproven")]
 use stm32l476rg::{interrupt, Interrupt};
 use stm32l476rg::Peripherals;
 
-use stm32l4x6_hal::gpio::{PA3,PC13};
+use stm32l4x6_hal::gpio::{PA5,PC13};
 use stm32l4x6_hal::*;
 
 use stm32l4x6_hal::common::Constrain;
@@ -40,7 +38,7 @@ fn main() -> ! {
     let mut gpioa = gpio::A::new(&mut rcc.ahb);
     let mut gpioc = gpio::C::new(&mut rcc.ahb);
 
-    let led:  PA3<gpio::Output<gpio::PushPull>> = gpioa.PA3.into_output(&mut gpioa.moder, &mut gpioa.otyper);
+    let mut led:  PA5<gpio::Output<gpio::PushPull>> = gpioa.PA5.into_output(&mut gpioa.moder, &mut gpioa.otyper);
     let btn1: PC13<gpio::Input<gpio::Floating>> = gpioc.PC13.into_input(&mut gpioc.moder, &mut gpioc.pupdr);
 
  //   let mut led: Led = gpioa
@@ -50,21 +48,14 @@ fn main() -> ! {
 
     let mut state = false;
     loop {
-      use stm32l4x6_hal::embedded_hal::digital::InputPin;
-
-         while btn1.is_high(){
+         while btn1.is_low(){
             ;
          }
-/*      while gpioc.idr.read().idr13().bit() {
-            
-        } 
-        while ! gpioc.idr.read().idr13().bit() {
-            
-        } 
-        // Toggle the state
-*/
+         while !btn1.is_low(){
+            ;
+         }
         state = !state;
-        use stm32l4x6_hal::embedded_hal::digital::OutputPin;
+        use embedded_hal::digital::OutputPin;
 
         if state {
             led.set_high(); 
