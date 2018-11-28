c3596af68d756ff96c6d27142949ac01a4f0b641
Fredrik Simonsson <simson@thesimson.net>
2018-11-28 14:20:31 +0900

    Added a transmit function for vector

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,26 @@
 #![no_main]
 // #![allow{dead_code}]
 
+#![feature(alloc)]
+#![feature(global_allocator)]
+#![feature(lang_items)]
+
+// Plug in the allocator crate
+extern crate alloc;
+//extern crate alloc_cortex_m;
+use core::alloc::Layout;
+
+#[macro_use]
+extern crate cortex_m_rt as rt; // v0.5.x
+
+use alloc::vec::Vec;
+use alloc_cortex_m::CortexMHeap;
+
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+
+
+
 // pick a panicking behavior
 extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // extern crate panic_abort; // requires nightly
@@ -25,6 +45,9 @@ use stm32l4x6_hal::rcc::clocking;
 use stm32l4x6_hal::time::{MegaHertz,Hertz};
 use stm32l4x6_hal::timer::*;
 use stm32l4x6_hal::*;
+use embedded_hal as hal;
+use embedded_hal::spi::{FullDuplex, Mode, Phase, Polarity};
+
 
 use stm32l4x6::TIM6;
 
@@ -33,13 +56,40 @@ use stm32l4x6_hal::common::Constrain;
 use embedded_hal::timer::{Periodic,CountDown};
 use embedded_hal::blocking::spi::Transfer;
 use cortex_m::asm::delay;
+fn spitransmit<S> (mut spi: S  , v1:Vec<u8>) -> (S,Vec<u8>)
+where
+    S: hal::spi::FullDuplex<u8>,
+{
+    let mut readdata = Vec::with_capacity(v1.len());
+
+    for elem in v1.iter() {
+       spi.send(*elem); 
+       let ans=block!(spi.read());
+       match ans {
+          Ok(t) => {
+             readdata.push(t);
+          }
+          Err(_) => {
+             while true{
+                //trap here
+             }
+          }
+       }
+    }
+    (spi,readdata)
+}
+
 
 #[entry]
-
 fn main() -> ! {
 
     let p = stm32l4x6::Peripherals::take().unwrap();
     let mut rcc = p.RCC.constrain();
+
+    // Initialize the allocator BEFORE you use it
+    let start = rt::heap_start() as usize;
+    let size = 1024; // in bytes
+    unsafe { ALLOCATOR.init(start, size) }
 
     let mut flash = p.FLASH.constrain();
 
@@ -57,32 +107,11 @@ fn main() -> ! {
 
     let mut timer: Timer<TIM6> = stm32l4x6_hal::timer::Timer::tim6(p.TIM6,Hertz(20), spiclocks, &mut rcc.apb1);
 
-/*  Added a blinky section here
-    let mut led:  PA5<gpio::Output<gpio::PushPull>> = gpioa.PA5.into_output(&mut gpioa.moder, &mut gpioa.otyper);
-    // TMR6 basic timer stm32l4x6::rcc::apb1enr1 | apb1rstr1
-    let mut timer: Timer<TIM6> = stm32l4x6_hal::timer::Timer::tim6(p.TIM6,Hertz(20), spiclocks, &mut rcc.apb1);
-
-    let mut state= false;
-    loop{
-       timer.start(1);
-       block!(timer.wait()); // blocks for 1 second
-       if state {
-          led.set_high();
-       }else{
-          led.set_low();
-       }
-       state=!state;
-       
-    }
-
-*/
     let spiclk:PA5<gpio::AF5> = gpioa.PA5.into_alt_fun(&mut gpioa.moder, &mut gpioa.afrl);
     let spimiso:PA6<gpio::AF5> = gpioa.PA6.into_alt_fun(&mut gpioa.moder, &mut gpioa.afrl);
     let spimosi:PA7<gpio::AF5> = gpioa.PA7.into_alt_fun(&mut gpioa.moder, &mut gpioa.afrl);
 
     let spifreq= stm32l4x6_hal::time::Hertz(2_000_000);
-
-
  
     let mut spi = spi::Spi::new(spi1,(spiclk,spimiso,spimosi),spifreq,embedded_hal::spi::MODE_0,&spiclocks,&mut rcc.apb2);
 
@@ -96,7 +125,6 @@ fn main() -> ! {
 
     let mut buf:[u8;10] = [0; 10];
 
- use stm32l4x6_hal::embedded_hal::spi::FullDuplex;
 
 
     spi_reset.set_low();
@@ -111,48 +139,34 @@ fn main() -> ! {
     loop {
        spi_cs.set_low();
        if spi_irq.is_low(){
-            spi.send(0xF8 as u8);
+          let mut v;
+          let ans = spitransmit(spi,[0xFC,0,0].to_vec());
+          spi = ans.0;
+          v = ans.1;
+          let ans = spitransmit(spi,[0xf8].to_vec());
+          spi = ans.0;
+          v = ans.1;
+
        }
        if spi_irq.is_high(){
-           spi.send(0x1C as u8);
-           spi.send(0x0 as u8); 
-           let ans=block!(spi.read());
-           match ans {
-               Ok(t) => {
-                   buf[0]=t;
-               }
-               Err(_) => {
-                   while true{
-                       //trap here
-                   }
-               }
-           }
-           let ans=block!(spi.read());
-           match ans {
-               Ok(t) => {
-                   buf[1]=t;
-               }
-               Err(_) => {
-                   while true{
-                       //trap here
-                   }
-               }
-           }
+          let mut v;
+          let ans = spitransmit(spi,[0x1C,0].to_vec());
+          spi = ans.0;
+          v = ans.1;
        }
        spi_cs.set_high();
 
 
-
-/*
-         //Read button for press and release
-         while spi_irq.is_high(){
-            delay(100);
-            ;
-         }
-         while spi_irq.is_low(){
-            delay(100);
-            ;
-         }
-*/
-    }
+      }
 }
+// required: define how Out Of Memory (OOM) conditions should be handled
+// *if* no other crate has already defined `oom`
+#[lang = "oom"]
+#[no_mangle]
+
+pub fn rust_oom(_layout: Layout) -> ! {
+   // trap here for the debuger to find
+   loop {
+   }
+}
+
