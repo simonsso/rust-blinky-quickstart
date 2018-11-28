3fa621ffcbccbe8b4012f7d4711f06f5e19edba1
Fredrik Simonsson <simson@thesimson.net>
2018-11-28 17:05:14 +0900

    New test vectors

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 // #![allow{dead_code}]
 
 #![feature(alloc)]
-#![feature(global_allocator)]
+// #![feature(global_allocator)]
 #![feature(lang_items)]
 
 // Plug in the allocator crate
@@ -56,12 +56,47 @@ use stm32l4x6_hal::common::Constrain;
 use embedded_hal::timer::{Periodic,CountDown};
 use embedded_hal::blocking::spi::Transfer;
 use cortex_m::asm::delay;
+
+
+fn spiread<S> (mut spi: S  ,l:u32) -> (S,Vec<u8>)
+where
+    S: hal::spi::FullDuplex<u8>,
+{   
+    let mut readdata: Vec<u8> = Vec::with_capacity(l as usize);
+    for _i in 0..l {
+       spi.send(0x00); 
+       let ans=block!(spi.read());
+       match ans { 
+          Ok(t) => {
+             readdata.push(t);
+          }
+          Err(_) => {
+             while true{
+                //trap here
+             }
+          }
+       }
+    }
+    readdata.push(0x99);
+    readdata.push(0x99);
+    readdata.push(0x99);
+    readdata.push(0x99);
+    readdata.push(0x99);
+    readdata.push(0x99);
+    readdata.push(0x99);
+    readdata.push(0x99);
+    readdata.push(0x99);
+    readdata.push(0x99);
+    readdata.push(0x99);
+    (spi,readdata)
+}
+
+
 fn spitransmit<S> (mut spi: S  , v1:Vec<u8>) -> (S,Vec<u8>)
 where
     S: hal::spi::FullDuplex<u8>,
 {
-    let mut readdata = Vec::with_capacity(v1.len());
-
+    let mut readdata: Vec<u8> = Vec::with_capacity(v1.len());
     for elem in v1.iter() {
        spi.send(*elem); 
        let ans=block!(spi.read());
@@ -76,6 +111,17 @@ where
           }
        }
     }
+    readdata.push(0x98);
+    readdata.push(0x98);
+    readdata.push(0x98);
+    readdata.push(0x98);
+    readdata.push(0x98);
+    readdata.push(0x98);
+    readdata.push(0x98);
+    readdata.push(0x98);
+    readdata.push(0x98);
+    readdata.push(0x98);
+    readdata.push(0x98);
     (spi,readdata)
 }
 
@@ -88,15 +134,16 @@ fn main() -> ! {
 
     // Initialize the allocator BEFORE you use it
     let start = rt::heap_start() as usize;
-    let size = 1024; // in bytes
-    unsafe { ALLOCATOR.init(start, size) }
+    let size = 102400; // in bytes
+
+       // Use SRAM2 for heap ans SRAM1 for stack
+    unsafe { ALLOCATOR.init(0x1000_0000 as usize, 0x8000 as usize) }
 
     let mut flash = p.FLASH.constrain();
 
     let cfgr = rcc.cfgr.sysclk(clocking::SysClkSource::MSI(clocking::MediumSpeedInternalRC::new(32_000_000, false))).hclk(MegaHertz(32)).pclk1(MegaHertz(32)).pclk2(MegaHertz(32));
     
     let spiclocks = cfgr.freeze(&mut flash.acr);
-
 
     let mut spi1 = p.SPI1;
 
@@ -120,44 +167,54 @@ fn main() -> ! {
     let mut spi_reset: PA10<gpio::Output<gpio::PushPull>> = gpioa.PA10.into_output(&mut gpioa.moder, &mut gpioa.otyper);
     let mut spi_cs:    PA9<gpio::Output<gpio::PushPull>> =  gpioa.PA9.into_output(&mut gpioa.moder, &mut gpioa.otyper);
     spi_cs.set_high();
+    spi_reset.set_high();
 
 
 
-    let mut buf:[u8;10] = [0; 10];
-
-
-
+    use embedded_hal::digital::InputPin;
     spi_reset.set_low();
     delay(1000);
     spi_reset.set_high();
-    use embedded_hal::digital::InputPin;
-    while spi_irq.is_low(){
-         delay(100);
-    }
-
     
     loop {
        spi_cs.set_low();
-       if spi_irq.is_low(){
-          let mut v;
-          let ans = spitransmit(spi,[0xFC,0,0].to_vec());
-          spi = ans.0;
-          v = ans.1;
-          let ans = spitransmit(spi,[0xf8].to_vec());
-          spi = ans.0;
-          v = ans.1;
-
-       }
-       if spi_irq.is_high(){
-          let mut v;
-          let ans = spitransmit(spi,[0x1C,0].to_vec());
-          spi = ans.0;
-          v = ans.1;
-       }
+       let ans = spitransmit(spi, [0x01,0x00,0x12,0x00,0x0c,0x00,0x01,0x00,0x01,0x00,0x02,0x40,0x02,0x00,0x09,0x10,0x00,0x00,0x07,0x00,0x00,0x00,0xb1,0x2e,0x45,0x93 ].to_vec());
+       spi = ans.0;
+       let v = ans.1;
        spi_cs.set_high();
 
+       while spi_irq.is_low(){
+       }
+       spi_cs.set_low();
+       let ans = spiread(spi,4);
+       spi = ans.0;
+       let v0 = ans.1;
+       spi_cs.set_high();
 
-      }
+       // expect magic 7f ff 01 7f
+       if ! (v0[0] == 0x7f && v0[1] == 0xff && v0[2] == 0x01 && v0[3] == 0x7f ) {
+         //handle error here
+       }
+       while spi_irq.is_low(){
+       }
+       spi_cs.set_low();
+       let ans = spiread(spi,4);
+       spi = ans.0;
+       let v1 = ans.1;
+       spi_cs.set_high();
+       let ans_c:u32 = v1[2] as u32;
+       if ans_c >0 && ans_c <256 {
+         // todo add add better check for this package
+       
+         while spi_irq.is_low(){
+         }
+          spi_cs.set_low();
+          let ans = spiread(spi,ans_c);
+          spi = ans.0;
+          let v2 = ans.1;
+          spi_cs.set_high(); 
+       }
+   }
 }
 // required: define how Out Of Memory (OOM) conditions should be handled
 // *if* no other crate has already defined `oom`
