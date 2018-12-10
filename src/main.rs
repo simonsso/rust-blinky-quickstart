f97ed9b6db6f42e2970a49d49e4a371acf3f8864
Fredrik Simonsson <simson@thesimson.net>
2018-12-10 13:15:19 +0900

    SPI confirmed with DMA interface

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@
 
 // Plug in the allocator crate
 extern crate alloc;
+extern crate nb;
 //extern crate alloc_cortex_m;
 use core::alloc::Layout;
 
@@ -16,6 +17,7 @@ extern crate cortex_m_rt as rt; // v0.5.x
 // use alloc::vec::Vec;
 use alloc_cortex_m::CortexMHeap;
 
+#[macro_use(block)]
 #[global_allocator]
 static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 
@@ -28,8 +30,6 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 extern crate nrf52832_hal;
 
-extern crate nb;
-
 use cortex_m_rt::entry;
 
 use nrf52832_hal::gpio::*;
@@ -38,6 +38,7 @@ use nrf52832_hal::gpio::p0::*;
 use nrf52832_hal::prelude::GpioExt;
 use nrf52832_hal::prelude::SpimExt;
 use embedded_hal::digital::{InputPin,OutputPin};
+use embedded_hal::spi::FullDuplex;
 
 // use nrf52_hal::spi;
 // use nrf52_hal::embedded_hal::digital::OutputPin;
@@ -45,6 +46,7 @@ use embedded_hal::digital::{InputPin,OutputPin};
 // use nrf52_hal::time::{MegaHertz,Hertz};
 // use nrf52_hal::timer::*;
 use nrf52832_hal::*;
+use nb::*;
 
 // use nrf52::TIM6;
 
@@ -77,10 +79,9 @@ fn main() -> ! {
 //    let mut _timer: Timer<TIM6> = nrf52_hal::timer::Timer::tim6(p.TIM6,Hertz(20), spiclocks, &mut rcc.apb1);
     let mut led0_red: P0_20<gpio::Output<PushPull>>  = port0.p0_20.into_push_pull_output(Level::Low );
 //Gpio Conflict
-    let mut led1_red: P0_19<gpio::Output<gpio::PushPull>>  = port0.p0_19.into_push_pull_output(Level::Low );
     let mut led2_green: P0_18<gpio::Output<PushPull>>  = port0.p0_18.into_push_pull_output(Level::Low );
     let mut led3_green: P0_17<gpio::Output<PushPull>>  = port0.p0_17.into_push_pull_output(Level::Low );
-//    let mut led1_red: P0_03<gpio::Output<PushPull>>  = port0.p0_03.into_push_pull_output(Level::Low );
+    let mut led1_red: P0_03<gpio::Output<PushPull>>  = port0.p0_03.into_push_pull_output(Level::Low );
 
     //arduino D13,D12,D11
     let spiclk:  P0_Pin<Output<PushPull>> = port0.p0_25.into_push_pull_output(Level::Low).degrade();
@@ -90,15 +91,34 @@ fn main() -> ! {
 //    let spifreq= nrf52_hal::time::Hertz(2_000_000);
 
     let pins = nrf52832_hal::spim::Pins{sck:spiclk,miso:spimiso,mosi:spimosi};
-    let mut spi0 = p.SPIM0.constrain(pins);
+    let mut spim: embedded_hal::spi::FullDuplex<Error> = p.SPIM0.constrain(pins);
 
+    let mut spi_cs = port0.p0_19.into_push_pull_output(Level::High ).degrade();
+    let mut spi_rst = port0.p0_13.into_push_pull_output(Level::High );
+    spi_rst.set_low();
+    delay(10);
+    spi_rst.set_high();
 
-    let btn1  = port0.p0_13.into_pullup_input();
+//    let btn1  = port0.p0_13.into_pullup_input();
     let btn2  = port0.p0_14.into_pullup_input();
     let btn3  = port0.p0_15.into_pullup_input();
     let btn4  = port0.p0_16.into_pullup_input();
 
+   
+    let mut txbuf:[u8;2] = [0x1c,00];
+    let mut rxbuf:[u8;2] = [0 ,0];
 
+    for i in 0..2 {
+        let _ans=block!(spim.send(txbuf[i]));
+        let ans:u8 = block!(spim.read()).unwrap();
+        rxbuf[i];
+    }
+
+
+    //let mut txbuf:[u8;3] = [0xfc,00,00];
+    // let mut rxbuf:[u8;3] = [0 ,0,0];
+
+    
     loop{
         led0_red.set_high();
         delay(1000000);
@@ -111,12 +131,12 @@ fn main() -> ! {
         led1_red.set_low();
         led2_green.set_low();
         led3_green.set_low();
-*/
         if btn1.is_high(){
             led0_red.set_high();
         }else{
             led0_red.set_low();
         }
+*/
         if btn2.is_high(){
             led1_red.set_high();
         }else{
